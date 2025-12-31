890050611dff5b7776bcf5ee31ad074573937822
agorapilled <johngardner13@outlook.com>
2025-12-31 11:10:45 -0500

    v0

--- app/page.tsx
+++ app/page.tsx
@@ -244,7 +244,7 @@ export default function Home() {
             </p>
           </div>
           
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
             {horses.map((horse) => (
               <HorseCard
                 key={horse.id}
